fa29e8fb381481935f6b904b98db3b66b7b04eba
Hasta Tri Angga <hasta.triangga@unimed.ac.id>
2021-05-22 15:30:08 +0700

    soal 1d terjawab

--- lab1/solution/solution1b.sh
+++ lab1/solution/solution1b.sh
@@ -3,12 +3,13 @@
 #copy dan ubah isi file
 for i in {1..5}
 do
-    if [[ $i = 1 ]]; then
+    if [[ $i == 1 ]]
+    then
         zip text.zip text1.txt
-    elif [[ $i > 1 ]]; then
+    else
         cp text1.txt text$i.txt
         sed -i -e "s/text1/text$i/g" text$i.txt
-        zip text.zip text$i.txt
+        zip text.zip text$i.txt 
     fi
 done
 
--- lab1/solution/solution1d.sh
+++ lab1/solution/solution1d.sh
@@ -1,22 +1,28 @@
 #!/bin/bash
 
+# zipping all the text and zip file recursively
 for i in {1..5}
 do
-    cp text1.txt text$i.txt
-    sed -i -e "s/text1/text$i/g" text$i.txt
-    zip 1.zip text$i.txt
-done
-
-for i in {1..5}
-do
-    if [[ $i>2 ]]; then
-        zip $i.zip $((i-1)).zip
+    if [[ $i == 1 ]]
+    then
+        zip ${i}.zip text1.txt
+    else
+        cp text1.txt text$i.txt
+        sed -i -e "s/text1/text${i}/g" text${i}.txt
+        zip ${i}.zip text$i.txt $((i-1)).zip
     fi
 done
 
+# cleanup temp files
 for i in {1..5}
 do
-    if [[ $i > 1 ]]; then
-        rm -rf text$i.txt
+    if [[ $i == 1 ]]
+    then
+        rm -f ${i}.zip
+    elif [[ $i == 5 ]]
+    then
+        rm -f text$i.txt
+    else
+        rm -rf ${i}.zip text$i.txt $((i-1)).zip
     fi
 done
